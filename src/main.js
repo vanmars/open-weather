7da65898cd4942844dfd02426ff5915d20d6fc21
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-09 20:06:37 -0700

    Replace fetch API with asyn functions

--- src/main.js
+++ src/main.js
@@ -11,26 +11,48 @@ function clearFields() {
   $('.showHumidity').text("");
   $('.showTemp').text("");
 }
-
 function getElements(response) {
   if (response.main) {
     $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
     $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
   } else {
-    $('.showErrors').text(`There was an error: ${response.message}`);
+    $('.showErrors').text(`There was an error: ${response}`);
   }
+}
+// New Way with Asyn Functions
+async function makeApiCall(city) {
+  const response = await WeatherService.getWeather(city);
+  getElements(response);
 }
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     clearFields();
-    WeatherService.getWeather(city)
-      .then(function(response) {
-        getElements(response);
-      });
+    makeApiCall(city);
   });
 });
+
+// Old Way with Fetch API
+// function getElements(response) {
+//   if (response.main) {
+//     $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+//     $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+//   } else {
+//     $('.showErrors').text(`There was an error: ${response.message}`);
+//   }
+// }
+
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     let city = $('#location').val();
+//     clearFields();
+//     WeatherService.getWeather(city)
+//       .then(function(response) {
+//         getElements(response);
+//       });
+//   });
+// });
 // Old Method with Promises
 // $(document).ready(function() {
 //   $('#weatherLocation').click(function() {
--- src/weather-services.js
+++ src/weather-services.js
@@ -1,17 +1,32 @@
+// New Way with Asyn Function
 export default class WeatherService {  
-  static getWeather(city) {
-    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
-      .then(function(response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch(function(error) {
-        return error;
-      })
+  static async getWeather(city) {
+    try {
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
     }
   }
+}
+// Old Way with Fetch API
+// export default class WeatherService {  
+//   static getWeather(city) {
+//     return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
+//       .then(function(response) {
+//         if (!response.ok) {
+//           throw Error(response.statusText);
+//         }
+//         return response.json();
+//       })
+//       .catch(function(error) {
+//         return error;
+//       })
+//     }
+//   }
 
 // Old way with Class Promise
 // export default class WeatherService {  
