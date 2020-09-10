5187d41c86f8d61e64a86d9f7eb49b10d6ba2c0f
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-09 20:31:19 -0700

    Update code to chain promises and make a second API call to Giphy

--- src/main.js
+++ src/main.js
@@ -2,36 +2,77 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import WeatherService from './weather-service.js'
+import WeatherService from './services/weather-service.js'
+import GiphyService from './services/giphy-service';
 
-// New Method
+
 function clearFields() {
   $('#location').val("");
   $('.showErrors').text("");
   $('.showHumidity').text("");
   $('.showTemp').text("");
 }
-function getElements(response) {
-  if (response.main) {
-    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
-    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-  } else {
-    $('.showErrors').text(`There was an error: ${response}`);
-  }
+
+function displayWeatherDescription(description) {
+  $('.weather-description').text(`The weather is ${description}!`);
 }
-// New Way with Asyn Functions
-async function makeApiCall(city) {
-  const response = await WeatherService.getWeather(city);
-  getElements(response);
+
+function displayGif(response) {
+  const url = response.data[0].images.downsized.url
+  $('.show-gif').html(`<img src='${url}'>`);
+}
+
+function displayErrors(error) {
+  $('.show-errors').text(`${error}`);
 }
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     clearFields();
-    makeApiCall(city);
+    WeatherService.getWeather(city)
+      .then(function(weatherResponse) {
+        if (weatherResponse instanceof Error) {
+          throw Error(`OpenWeather API error: ${weatherResponse.message}`);
+        }
+        const weatherDescription = weatherResponse.weather[0].description;
+        displayWeatherDescription(weatherDescription);
+        return GiphyService.getGif(weatherDescription);
+      })
+      .then(function(giphyResponse) {
+        if (giphyResponse instanceof Error) {
+          throw Error(`Giphy API error: ${giphyResponse.message}`);
+        }
+        displayGif(giphyResponse);
+      })
+      .catch(function(error) {
+        displayErrors(error.message)
+      })
   });
 });
+
+// Old Method w/out Chaining
+// function getElements(response) {
+//   if (response.main) {
+//     $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+//     $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+//   } else {
+//     $('.showErrors').text(`There was an error: ${response}`);
+//   }
+// }
+// // New Way with Asyn Functions
+// async function makeApiCall(city) {
+//   const response = await WeatherService.getWeather(city);
+//   getElements(response);
+// }
+
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     let city = $('#location').val();
+//     clearFields();
+//     makeApiCall(city);
+//   });
+// });
 
 // Old Way with Fetch API
 // function getElements(response) {
